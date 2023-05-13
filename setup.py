17b1572712534a4585d0b99b8235236e4c762a6c
塵渊 <britton@126.com>
2023-05-13 18:41:27 +0800

    python构建打包工具新增源文件

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, Extension
 
-setup(name="PythonExtention", version="1.0", ext_modules=[Extension("PythonExtention", sources=["dllmain.c"])])
+setup(name="PythonExtention", version="1.0", ext_modules=[Extension("PythonExtention", sources=["dllmain.c","PythonExtention.cpp"])])
