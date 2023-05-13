2e22a0a2cb7e0a27a05f1cf98ecf3f60124c1979
塵渊 <britton@126.com>
2023-05-13 18:55:52 +0800

    工程配置新增Python源码目录 完善打包文件的配置

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, Extension
 
-setup(name="PythonExtention", version="1.0", ext_modules=[Extension("PythonExtention", sources=["dllmain.c","PythonExtention.cpp"])])
+setup(name="PythonExtention", version="1.0", ext_modules=[Extension("PythonExtention", sources=["dllmain.cpp","pch.cpp","PythonExtention.cpp"])])
